6fd3a879e86ad45a8f70ce2cb895a1ec463b5d1d
Smiley32 <jeannin.emile@laposte.net>
2018-01-18 10:46:09 +0100

    implem d'une version de get_message sans gérer les concaténations

--- message.cpp
+++ message.cpp
@@ -4,10 +4,70 @@
 #include "packet.h"
 #include "message.h"
 
+boost::array<char, 128> get_char_message(tcp::socket &socket, size_t &length) {
+  boost::array<char, 128> buf;
+  boost::system::error_code error;
 
-Message get_message(tcp::socket *socket)
+  length = socket.read_some(boost::asio::buffer(buf), error);
+
+  if(error) {
+    buf[0] = -1;
+  }
+
+  return buf;
+}
+
+Message get_message(tcp::socket &socket)
 {
+  size_t length;
+  boost::array<char, 128> msg = get_char_message(socket, length);
+
   struct Message new_message;
+
+  switch(msg[ID_INDEX]) {
+    case ID_message::Accept:
+      new_message.id = ID_message::Accept;
+      new_message.data.accept = msg[1] == 1;
+      break;
+    case ID_message::Initiate:
+      new_message.id = ID_message::Initiate;
+      int piece_index = 0;
+      for(int i = 1; i < PLAYER_MAX_PIECES*2 + 1; i += 2) {
+        new_message.data.initiate.pieces[piece_index].pos = (int)(msg[i]);
+        new_message.data.initiate.pieces[piece_index].value = (int)(msg[i + 1]);
+        piece_index++;
+      }
+      break;
+    case ID_message::Play:
+      new_message.id = ID_message::Play;
+      break;
+    case ID_message::Move:
+      new_message.id = ID_message::Move;
+      new_message.move.source = (int)(msg[1]);
+      new_message.move.target = (int)(msg[2]);
+      break;
+    case ID_message::Update:
+    {
+      new_message.id = ID_message::Update;
+      new_message.update.collision = msg[UPDATE_INDEX] == COLLISION;
+
+      new_message.update.movement.source = msg[2];
+      new_message.update.movement.target = msg[3];
+
+      if(new_message.update.collision) {
+        new_message.udpate.enemy_value = msg[4];
+        new_message.update.result = (Result)(msg[5]);
+      }
+    }
+    break;
+    case ID_message::End:
+      new_message.id = ID_message::End;
+      new_message.end = (Result)(msg[1]);
+      break;
+    case ID_message::Quit:
+      new_message.id = ID_message::Quit;
+      break;
+  }
 
   return new_message;
 }
--- message.h
+++ message.h
@@ -1,6 +1,13 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#define ID_INDEX 0
+
+#define PLAYER_MAX_PIECES 40
+
+#define UPDATE_INDEX 1
+#define COLLISION 1
+
 enum class ID_message : int
 {
   Accept = 0,
@@ -12,43 +19,65 @@ enum class ID_message : int
   Quit = 6
 };
 
+enum class Result : int
+{
+  Lose = 0,
+  Win = 1,
+  Lose = 0
+};
+
+typedef struct Movement Movement;
 struct Movement
 {
   int source;
   int target;
 };
 
+typedef struct Resumed_piece Resumed_piece;
 struct Resumed_piece
 {
   int pos;
   int value;
 };
 
+typedef struct Initialize Initialize;
 struct Initialize
 {
-  Resumed_piece pieces[40];
+  Resumed_piece pieces[PLAYER_MAX_PIECES];
 };
 
+typedef struct Update Update;
 struct Update
 {
   bool collision;
   Movement movement;
   int enemy_value;
-  int result;
+  Result result;
 };
 
+typedef struct Message Message;
 struct Message
 {
   ID_message id;
   union data
   {
-    bool result;
-    Movement movement;
-    Initialize initialize;
+    bool accept;
+    Initialize initiate;
+    // play: rien
+    Movement move;
+    Update update;
+    Result end;
+    // quit: rien
   };
 };
 
-Message get_message(tcp::socket *socket);
+/**
+ * Attends un message (ne gère pas la réception de messages concaténés)
+ * 
+ * @param tcp::socket *socket Socket sur laquelle lire le message
+ * @return Message    Message retourné
+ */
+Message get_message(tcp::socket &socket);
 void get_vector_coord(gf::Vector2u *coo2D, int piece_pos, bool inversed);
 int get_pos_from_vector(gf::Vector2u *coo2D, bool inversed);
 
